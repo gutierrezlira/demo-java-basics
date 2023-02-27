f2a3c0202f999949d0abcde6acb9da73f899a59d
Alina Darenskikh <alinadarenskikh@gmail.com>
2023-02-27 14:26:47 +0300

    add AreaOfACircle

---
+++ src/main/java/by/itacademy/alinadarenskikh/javabasics/AreaOfACircle.java
@@ -0,0 +1,16 @@
+package by.itacademy.alinadarenskikh.javabasics;
+
+public class AreaOfACircle {
+    public static void main(String[] args) {
+
+        double radius = 5.0;
+        double area = calculateCircleArea(radius);
+
+        System.out.println("Площадь круга с радиусом " + radius + " равна " + area);
+    }
+
+    public static double calculateCircleArea(double radius) {
+
+        return Math.PI * Math.pow(radius, 2);
+    }
+}
